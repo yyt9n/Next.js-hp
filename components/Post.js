cf897b20e88c03709cfa7085850d4036c24f178f
yuto <h.yyt9n@gmail.com>
2022-09-24 13:12:39 +0900

    getStaticPaths(Dynamic routes)実装

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 const Post = ({ post }) => {
     return (
         <div>
             <span>{post.id}</span>
             {" : "}
-            <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
-                {post.title}
-            </span>
+            <Link href={`/posts/${post.id}`}>
+                <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+                    {post.title}
+                </span>
+            </Link>
         </div>
     )
 }
