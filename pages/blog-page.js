929f4b639aba3752c16c679587d6f38684307e50
yuto <h.yyt9n@gmail.com>
2022-09-24 11:13:34 +0900

    Layoutページ実装、Contactページ実装

---
+++ pages/blog-page.js
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout"
+
+const Blog = () => {
+    return <Layout title="Blog">Blog page</Layout>
+}
+
+export default Blog
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+    <Layout title="Home">
+      <p className="text-4xl">Welcome to Nextjs</p>
+    </Layout>
   )
 }
